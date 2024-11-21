10a1a6c9da2b1dd7e1b0239aaa09f7988ffeb383
Audora <gy2553@naver.com>
2024-11-22 07:08:36 +0900

    초기설정

---
+++ src/main/java/com/audora/lotting_be/LottingBeApplication.java
@@ -0,0 +1,13 @@
+package com.audora.lotting_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LottingBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/audora/lotting_be/LottingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audora.lotting_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottingBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
